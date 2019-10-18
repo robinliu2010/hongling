923d80953e50b7e8f99c9e4392788b20c6b0dccf
robinliu2010 <robinliu2010@163.com>
2019-10-18 19:08:45 +0800

    添加测试数据

--- hongling/scripts/crawl_shiyanlou_courses.py
+++ hongling/scripts/crawl_shiyanlou_courses.py
@@ -6,9 +6,9 @@ class CoursesSpider(scrapy.Spider):
     start_urls = ['https://www.shiyanlou.com/bootcamp/']
 
     def parse(self, response):
-        for course in response.css('div.bootcamp-course-item'):
+        for course in response.css('div.col-3'):
             yield {
-                'name': course.xpath('.//div[@class="course-title"]/a/span/text()').extract_first().strip(),
-                'description': course.xpath('.//div[@class="course-desc"]/a/p/text()').extract_first().strip(),
-                'image_url': course.xpath('.//div[@class="course-img"]/a/img/@src').extract_first()
+                'name': course.css('div.course-body p::text').extract_first().strip(),
+                'description': course.xpath('.//div[@class="course-body"]/p[2]/text()').extract_first(),
+                'image_url': course.css('img::attr(src)').extract_first()
             }
